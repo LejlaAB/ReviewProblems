9af3f25fffe83cb4b5238fe1bd491714b05a327b
lejla.al-buslimi <s235661@dtu.dk>
2025-11-18 16:26:10 +0100

    test

---
+++ DigitSumForeach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter an integer");
+        var input = Console.ReadLine();
+
+        int sum = 0;
+
+        foreach (var ch in input)
+        {
+            int digit = int.Parse(ch.ToString());
+            sum = sum + digit;
+        }
+
+        Console.WriteLine("Sum: " + sum);
+    }
+}
---
+++ DigitSumModulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter an integer");
+        var text = Console.ReadLine();
+        int number = int.Parse(text);
+
+        int sum = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+
+        Console.WriteLine("Sum: " + sum);
+    }
+}
